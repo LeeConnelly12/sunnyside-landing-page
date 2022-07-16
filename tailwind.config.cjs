fe839e4ed0be28747865a188d8a43dbcea666e62
Lee Connelly <lee@leeconnelly.dev>
2022-07-16 03:46:18 +0100

    Add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./index.html', './main.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Barlow', ...defaultTheme.fontFamily.sans],
+        fraunces: ['Fraunces', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
