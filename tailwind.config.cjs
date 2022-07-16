f004b1487980e715978c9b2070bf9ec21f40c893
Lee Connelly <lee@leeconnelly.dev>
2022-07-16 10:06:01 +0100

    Create grid section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,18 @@ module.exports = {
         sans: ['Barlow', ...defaultTheme.fontFamily.sans],
         fraunces: ['Fraunces', ...defaultTheme.fontFamily.serif],
       },
+      colors: {
+        gray: {
+          700: 'hsl(232, 10%, 55%)',
+          900: 'hsl(212, 27%, 19%)',
+        },
+        yellow: {
+          500: 'hsl(51, 100%, 49%)',
+        },
+        red: {
+          500: 'hsl(7, 99%, 70%)',
+        },
+      },
     },
   },
   plugins: [],
